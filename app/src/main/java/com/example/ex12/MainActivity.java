62ea0a58971d1688ca5491d0adfc664119818745
gl7857 <gl7857@bs.amalnet.k12.il>
2024-04-07 23:52:38 +0300

    ex12

--- app/src/main/java/com/example/ex12/MainActivity.java
+++ app/src/main/java/com/example/ex12/MainActivity.java
@@ -9,15 +9,6 @@ import android.widget.EditText;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.view.View;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
     EditText edt1;
     double num1 = 0, result = 0;
